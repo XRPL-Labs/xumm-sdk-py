f1664cedbccf3670c0236756ea920e2d3ffd035c
Denis Angell <denis@harpangell.com>
2022-01-21 00:06:37 -0500

    add-debug

--- xumm/__init__.py
+++ xumm/__init__.py
@@ -8,6 +8,7 @@ api_key = os.environ.get('XUMM_APIKEY', None)
 api_secret = os.environ.get('XUMM_APISECRET', None)
 api_version = 'v1'
 env = 'production'
+debug = True if os.environ.get('DEBUG') == 'xumm-sdk*' else False  # noqa: E501
 
 from xumm.resource.base import (  # noqa
     XummSdk,
