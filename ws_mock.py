f625b3703bd959b02f844ce5d5414369d7f78d23
Denis Angell <denis@harpangell.com>
2022-01-20 23:40:43 -0500

    tmp mock ws

---
+++ ws_mock.py
@@ -0,0 +1,43 @@
+
+import asyncio
+import json
+from unittest.mock import Mock, patch  # noqa: E501
+from tests.fixtures import xumm_api as test_fixtures
+from xumm.util import read_json
+
+import websockets
+
+json_fixtures = read_json('./tests/fixtures/xumm_api.json')
+
+async def start_server(ws, path):
+    try:
+        print('MOCK SOCKET OPEN: {}'.format(ws.open))
+
+        await ws.send(json.dumps(test_fixtures.subscription_updates()['expire']))
+        print('SENT EXPIRE')
+        print(test_fixtures.subscription_updates()['expire'])
+
+        await ws.send(json.dumps(test_fixtures.subscription_updates()['opened']))
+        print('SENT OPENED')
+        print(test_fixtures.subscription_updates()['opened'])
+
+        await ws.send(json.dumps(test_fixtures.subscription_updates()['rejected']))
+        print('SENT REJECTED')
+        print(test_fixtures.subscription_updates()['rejected'])
+
+        await asyncio.sleep(1)
+
+    except KeyboardInterrupt as e:
+        ws.close()
+        
+    except Exception as e:
+        print('on_open Error: {}'.format(e))
+        ws.close()
+
+async def main():
+    async with websockets.serve(start_server, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
